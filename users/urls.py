53d504f80be5331d5bc3532c333c938a0e1f7cf2
Devin Jackson <devin@Devins-MBP.home>
2019-11-04 00:11:19 -0500

    get user

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('password/reset/', views.ChangePasswordView.as_view()),
     re_path('profiles/(?P<id>[0-9]+)/$', views.ProfileDetailView.as_view()),
     path('profiles/', views.ProfileListView.as_view()),
-    re_path('(?P<id>[0-9]+)/$', views.UserDetailView.as_view()),
+    re_path('(?P<id>[0-9a-zA-Z]+)/$', views.UserDetailView.as_view()),
     path('', views.UserListView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from . import serializers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 from . import models
 from . import serializers
@@ -18,11 +19,22 @@ class UserListView(generics.ListCreateAPIView):
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
     pagination_class = StandardResultsSetPagination
+    authentication_classes = (TokenAuthentication,)
 
 class UserDetailView(generics.RetrieveAPIView):
+    
     lookup_field = "id"
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def retrieve(self, request, id=None):
+        """
+        If provided 'pk' is "me" then return the current user.
+        """
+        if request.user and id == 'me':
+            return Response(serializers.UserSerializer(request.user).data)
+        return super(UserDetailView, self).retrieve(request, id)
 
 
 
@@ -33,6 +45,7 @@ class ChangePasswordView(generics.UpdateAPIView):
         serializer_class = serializers.ChangePasswordSerializer
         model = models.CustomUser
         permission_classes = (IsAuthenticated,)
+        authentication_classes = (TokenAuthentication,)
 
         def get_object(self, queryset=None):
             obj = self.request.user
@@ -57,8 +70,10 @@ class ProfileListView(generics.ListCreateAPIView):
     serializer_class = serializers.ProfileSerializer
     pagination_class = StandardResultsSetPagination
     filterset_fields = [f.name for f in models.BusinessProfile._meta.fields]
+    authentication_classes = (TokenAuthentication,)
 
 class ProfileDetailView(generics.RetrieveAPIView):
     lookup_field = "id"
     queryset = models.BusinessProfile.objects.select_related('user').all()
     serializer_class = serializers.ProfileSerializer
+    authentication_classes = (TokenAuthentication,)
