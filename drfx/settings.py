9718603d51301604d8ffd0178526e89cf3b67c06
Devin Jackson <devin@devins-mbp.lan>
2019-11-20 10:26:30 -0500

    added to cors whitelist

--- drfx/settings.py
+++ drfx/settings.py
@@ -97,12 +97,12 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100
 }
 
-CORS_ORIGIN_WHITELIST = [
-    "https://jamtechnyc.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-    "https://pacific-earth-00442.herokuapp.com/"
-]
+# CORS_ORIGIN_WHITELIST = [
+#     "https://jamtechnyc.com",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000"
+#     "https://pacific-earth-00442.herokuapp.com/"
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
